942347b2c07abc1c30be1862b45ec32ab5d2fff7
jayasankarj93 <68055360+jayasankarj93@users.noreply.github.com>
2022-10-23 21:29:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ if config.BOT_TOKEN:
         api_hash=config.API_HASH,
         bot_token=config.BOT_TOKEN,
     )
-    app = Client(config.SESSION, api_id=config.API_ID, api_hash=config.API_HASH)
-
+    
     client = bot
 else:
     client = app
